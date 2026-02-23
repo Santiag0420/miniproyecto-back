3d50b0d7e29452499470b4f5805a70fbe85c40f7
tormenask <santiagoballa2003@gmail.com>
2026-02-23 16:57:19 -0500

    fix: se arregla el tipo de id del usuario a bigauto por uuid

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Usuario(models.Model):
-    id = models.BigAutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True)
     created_at = models.DateTimeField()
     name = models.CharField(max_length=100)
     age = models.IntegerField()
